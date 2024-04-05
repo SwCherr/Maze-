cd3fe95ff64796533e8623a45b58332c2ffa2fb7
Eros Rolfe <erosrolf@pr-m5.kzn.21-school.ru>
2024-04-05 14:45:37 +0300

    added CMakeLists, added test window

--- src/src/main.cc
+++ src/src/main.cc
@@ -1,1 +1,27 @@
+#include <SFML/Graphics.hpp>
 
+int main() {
+  // Создаем главное окно
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Maze");
+
+  // Главный цикл программы: выполняется, пока открыто окно
+  while (window.isOpen()) {
+    // Обработка событий
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // Пользователь нажал на «закрыть»: выходим
+      if (event.type == sf::Event::Closed) window.close();
+    }
+
+    // Очищаем экран
+    window.clear();
+
+    // Здесь можно рисовать объекты
+    // ...
+
+    // Отображаем содержимое буфера на экран
+    window.display();
+  }
+
+  return 0;
+}
