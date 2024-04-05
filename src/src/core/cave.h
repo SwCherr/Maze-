0bbe0a06838d7a964bd54b62197a21c5fed1233d
Eros Rolfe <erosrolf@pr-m5.kzn.21-school.ru>
2024-04-05 12:16:52 +0300

    added cave.stepOfGeneration()

--- src/src/core/cave.h
+++ src/src/core/cave.h
@@ -2,22 +2,32 @@
 #define A1_CAVE_CPP_CORE_CAVE_H_
 
 #include <iostream>
+
 #include "abstract_grid.h"
 
 namespace s21 {
 
 class Cave : public AbstractGrid {
  public:
+  enum class GenerationResult { kStable, kMutation, kInvalidParams };
+  using Matrix = std::vector<std::vector<bool>>;
+
   Cave() {}
+  Cave(int rows, int cols) : data_(rows, std::vector<bool>(cols, false)) {}
+  Cave(Matrix& other_data) : data_(other_data) {}
   ~Cave() {}
 
+  Matrix getData() const;
   int getRows() const noexcept;
   int getCols() const noexcept;
   bool initFromFile(const std::string& filename);
+  int numberOfNeighbors(int row, int col, const Matrix& matrix);
+  GenerationResult stepOfGeneration(int birth_limit, int death_limit);
+  // int cycleOfGeneration(int birth_limit, int death_limit);
 
  private:
   Matrix data_;
 };
-}
+}  // namespace s21
 
-#endif
+#endif  // A1_CAVE_CPP_CORE_CAVE_H_
--- src/tests/cave_test.cc
+++ src/tests/cave_test.cc
@@ -1,9 +1,20 @@
 #include "../src/core/cave.h"
+
 #include <gtest/gtest.h>
 
-TEST(Cave, cave_simple_test) {
+TEST(Cave, initFromFile) {
   s21::Cave cave;
   cave.initFromFile("tests/resources/cave01");
   ASSERT_EQ(cave.getRows(), 4);
   ASSERT_EQ(cave.getCols(), 4);
+}
+
+TEST(Cave, numberOfNeighbors_01) {
+  s21::Cave cave(1, 1);
+  ASSERT_EQ(cave.numberOfNeighbors(0, 0, cave.getData()), 8);
+}
+
+TEST(Cave, numberOfNeighbors_02) {
+  s21::Cave cave(3, 3);
+  ASSERT_EQ(cave.numberOfNeighbors(1, 1, cave.getData()), 0);
 }
